252765335135996e7e92354548539b4fea4f714d
Tylar <mail@tylar.info>
2023-10-24 14:03:52 -0400

    + curl submit command

--- airflow/dags/sofar_ingest.py
+++ airflow/dags/sofar_ingest.py
@@ -29,23 +29,33 @@ with DAG(
         task_id=f"sofar_ingest",
         bash_command=(
             "curl 'https://api.sofarocean.com/fetch/download-sensor-data/?spotterId=SPOT-30987C&startDate={{params.EXECUTION_DATE}}Z&endDate={{params.PREVIOUS_DAY}}Z&processingSources=all' "
-            "-X GET "
-            "-H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'"
-            "-H 'Accept: application/json, text/plain, */*'"
-            "-H 'Accept-Language: en-US,en;q=0.5'"
-            "-H 'Content-Type: application/x-www-form-urlencoded'"
-            "-H 'view_token: 1bc9848d3e524c34a1eb220e121d9a9e'"
-            "-H 'Sec-Fetch-Dest: empty'"
-            "-H 'Sec-Fetch-Mode: cors'"
-            "-H 'Sec-Fetch-Site: same-site'"
-            "-H 'Pragma: no-cache'"
-            "-H 'Cache-Control: no-cache'"
-            "-H 'referrer: https://spotters.sofarocean.com/'"
-            "-H 'credentials: omit'"
-            "-H 'mode: cors'"
+            "  -X GET "
+            "  -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0' "
+            "  -H 'Accept: application/json, text/plain, */*' "
+            "  -H 'Accept-Language: en-US,en;q=0.5' "
+            "  -H 'Content-Type: application/x-www-form-urlencoded' "
+            "  -H 'view_token: 1bc9848d3e524c34a1eb220e121d9a9e' "
+            "  -H 'Sec-Fetch-Dest: empty' "
+            "  -H 'Sec-Fetch-Mode: cors' "
+            "  -H 'Sec-Fetch-Site: same-site' "
+            "  -H 'Pragma: no-cache' "
+            "  -H 'Cache-Control: no-cache' "
+            "  -H 'referrer: https://spotters.sofarocean.com/' "
+            "  -H 'credentials: omit' "
+            "  -H 'mode: cors' "
+            "  > datafile.csv "
+            " && head datafile.csv "
+            " && curl --location --fail-with-body "
+            "    --form measurement=sofar_bouy "
+            "    --form tag_set=spotter_id=SPOT30987C "
+            "    --form fields=value,sensor_position "
+            "    --form time_column=utc_timestamp "
+            "    --form file=@./datafile.csv "
+            "    {{params.uploader_route}} "
         ),
         params={
             'EXECUTION_DATE': '{{ ds }}T{{ ts }}',
             'PREVIOUS_DAY': '{{ ds - timedelta(days(-1)) }}T{{ ts}}',
+            'uploader_route': UPLOADER_ROUTE,
         }
     )
