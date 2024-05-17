b6db6669a965dae3874a2fe776c4adc5ba4aa2db
Tylar <mail@tylar.info>
2024-05-17 13:32:21 -0400

    Delete influxdb/initdb/02_add_data.sh

--- influxdb/initdb/02_add_data.sh
+++
@@ -1,4 +0,0 @@
-
-# === create data like this:
-# curl -i -XPOST 'http://localhost:8086/write?db=mydb' \
-# --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
