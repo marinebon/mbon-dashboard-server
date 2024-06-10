8ebff657ceb9ef9b9665f632368067bce5a0f356
Tylar <mail@tylar.info>
2024-06-10 10:15:22 -0400

    get hostname from env, localhost doesn't work.

--- airflow/dags/nerr_data_ingest.py
+++ airflow/dags/nerr_data_ingest.py
@@ -42,7 +42,7 @@ with DAG(
         
         token = os.environ.get("INFLUXDB_TOKEN")
         org = "imars"
-        url = "http://localhost:8086"
+        url = os.eviron.get("INFLUXDB_HOSTNAME")
         
         client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
         bucket="imars-bucket"
