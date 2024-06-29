9aba6a4115fd63eed1b06e315dab6d0146730afc
Tylar Murray <murray.tylar@gmail.com>
2024-06-29 19:23:51 +0000

    mv csv2influx fn into file

--- airflow/dags/usgs_gh_ingest.py
+++ airflow/dags/usgs_gh_ingest.py
@@ -21,53 +21,8 @@ with DAG(
         #'retry_delay': timedelta(days=1),
     },
 ) as dag:
-    def data2influx(data_url, measurement, tags=[], field=["value", "Sal"], timeCol='DateTimeStamp'):
-        """
-        fetch data from IMaRS gcloud bucket
-        """
-        import pandas as pd
-        try: 
-            data = pd.read_csv(data_url)
-            print(f"loaded data cols: {data.columns}")
-            print(f"1st few rows:\n {data.head()}")
-        except Exception as e:
-            print(f"failed to `getData({data_url})`...\n", e)
-            raise e
-        
-        # === upload the data
-        # influx connection setup
-        import influxdb_client, os, time
-        from influxdb_client import InfluxDBClient, Point, WritePrecision
-        from influxdb_client.client.write_api import SYNCHRONOUS
-        
-        token = os.environ.get("INFLUXDB_TOKEN")
-        org = "imars"
-        url = os.environ.get("INFLUXDB_HOSTNAME")
-        
-        client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-        bucket="imars_bucket"
-        
-        # write each point in the df to influxDB
-        points = []
-        for index, row in data.iterrows():
-            print(f"{row['Sal']} @ {row['DateTimeStamp']}")
-            point = (
-                Point(measurement)
-                .time(row[timeCol])  # not utc_timestamp ?
-            )
-            for key, val in fields:
-                point = point.field(key, row[val])
-            for key, val in tags:
-                point = point.tag(key, val)
-            points.append(point)
-        
-        # Batch write points
-        results = client.write_api(write_options=SYNCHRONOUS).write(bucket=bucket, org=org, record=points)  
-        # Manually close the client to ensure no batching issues
-        client.__del__()
-        print("influxdb API response:")
-        print(results)
-
+    from csv2influx import csv2influx
+    
     GBUCKET_URL_PREFIX = "https://storage.googleapis.com/dashboards_csvs"
     FILE_LIST = {
         'gh': [
@@ -86,7 +41,7 @@ with DAG(
             DATA_FNAME = f"USGS_{param}_{location}.csv"
             PythonOperator(
                 task_id=f"ingest_usgs_{param}_{location}",
-                python_callable=data2influx,
+                python_callable=csv2influx,
                 op_kwargs={
                     'data_url': f"{GBUCKET_URL_PREFIX}/{DATA_FNAME}",
                     'measurement': param,
