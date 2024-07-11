9a81e8be93fbaf4703eaf90c6c6c6535ddfea0a3
Tylar Murray <murray.tylar@gmail.com>
2024-07-11 17:46:31 +0000

    fix all the errors

--- airflow/dags/ingest_nerrs_met.py
+++ airflow/dags/ingest_nerrs_met.py
@@ -15,10 +15,10 @@ from datetime import datetime, timedelta
 
 from nerrs2influx import nerrs2influx
 
-suite = 'met'
+SUITE = 'met'
 # station list from                                                                               
 #    python3 -c 'from nerrs_data.exportStationCodes import exportStationCodesDictFor; exportStationCodesDictFor("sap")'                                                                                
-stations = [
+STATIONS = [
     {"NERR_SITE_ID":"sap","Station_Code":"sapmlmet","Station_Name":"Marsh Landing","status":"Active"\
 ,"active_dates":"Sep 2002-","state":"ga","reserve_name":"Sapelo Island","params_reported":["ATemp","R\
 H","BP","WSpd","MaxWSpd","MaxWSpdT","Wdir","SDWDir","TotPrcp","TotPAR","CumPrcp","TotSoRad"],"Real_ti\
@@ -36,22 +36,19 @@ with DAG(
     max_active_runs=1,
     default_args={
         "start_date": datetime(2020, 1, 1),
-        #'retries': 3,
-        #'retry_delay': timedelta(days=1),
     },
 ) as dag:
-    # example path: `SAP_CabCr_Sal_NERR_WQ_HIST_SEUSdb.csv`
-    NERR_FPATH = "SAP_{station_name}_{product}_NERR_{suite}_HIST_SEUSdb.csv"
-    for station in stations:
-        for param in station['parameters_reported']:
+    for station in STATIONS:
+        for param in station['params_reported']:
+            station_name = station['Station_Name'].replace(' ', '_')
             PythonOperator(
-                task_id=f"ingest_nerrs_{suite}_{product}_{station_name}",
+                task_id=f"ingest_nerrs_{SUITE}_{param}_{station_name}",
                 python_callable=nerrs2influx,
                 op_kwargs={
-                    'station_name': station['Station_Name'],
-                    'station_code': station['NERR_SITE_ID'],  # "acespwq"  # ace sp wq                   
-                    'suite': ,
-                    'product': product # "Sal"                                                
+                    'station_name': station_name,
+                    'station_code': station['NERR_SITE_ID'],  # "acespwq"  # ace sp wq               
+                    'suite': SUITE,
+                    'product': param # "Sal"                                                
                 },
             )
             
