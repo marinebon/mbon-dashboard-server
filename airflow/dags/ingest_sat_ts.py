14cae3c403ca4d71b5f04dc7ee5416e869d391e9
Tylar Murray <murray.tylar@gmail.com>
2024-07-24 16:56:32 +0000

    update to use VIIRS files

--- airflow/dags/ingest_sat_ts.py
+++ airflow/dags/ingest_sat_ts.py
@@ -43,16 +43,20 @@ with DAG(
     ]
     SAT_FILE_DETAIL_LIST = [
         # sat    | product
-        ["MODA", "chlor_a"],
-        ["MODA", "Rrs_667"],
-        ["MODA", "Kd_490"],
-        ["MODA", "sst4"],
-        ["MODA",  "ABI"],
+        ['VSNPP', 'Kd_490'],
+        ['VSNPP', 'Rrs_671'],
+        ['VSNPP', 'chlor_a'],
+        ['VSNPP', 'sstn'],
+        #["MODA", "chlor_a"],
+        #["MODA", "Rrs_667"],
+        #["MODA", "Kd_490"],
+        #["MODA", "sst4"],
+        #["MODA",  "ABI"],
     ]
     for roi in SAT_ROI_LIST:
         for sat, product in SAT_FILE_DETAIL_LIST:
-            # example path: `GOMdbv2_ABI_TS_MODA_daily_Alderice.csv`
-            DATA_FNAME = f"{REGION}dbv23_{product}_TS_{sat}_daily_{roi}.csv"
+            # example path: `SEUS_Kd_490_TS_VSNPP_daily_01.csv`
+            DATA_FNAME = f"{REGION}_{product}_TS_{sat}_daily_{roi}.csv"
             PythonOperator(
                 task_id=f"ingest_sat_{roi}_{sat}_{product}",
                 python_callable=csv2influx,
