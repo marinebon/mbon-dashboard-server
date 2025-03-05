269a3ac5e90ef80d52c6b754d8b550e3abc9721d
Tylar Murray <email@tylar.info>
2025-03-05 18:19:11 +0000

    modify to use new buckets

--- airflow/dags/ingest_usgs_guages.py
+++ airflow/dags/ingest_usgs_guages.py
@@ -1,21 +1,66 @@
+# ingest_usgs_gauges.py
 """
-Ingest of USGS discharge and guage height data.
+Ingest of USGS discharge and gauge height data.
 
-```mermaid
+mermaid:
 usgs 
   -- ??? --> gbucket 
-  -- "airflow ingest_USGS_guages" --> influxDB
-```
+  -- "airflow ingest_USGS_gauges" --> influxDB
 """
 import os
-
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
-# These jobs run once to grab all the data.
-# TODO: modify to only grab latest data.
+from csv2influx import csv2influx
+
+USGS_DB_FILES = {
+    'SEUS': {
+        'locations': [
+            'SavannahRv_SEUSdb', 'HudsonCr_SEUSdb', 'AltamahaRv_SEUSdb', 'SatillaRv_SEUSdb',
+            'StJohnsRv_SEUSdb', 'OgeecheeRv_SEUSdb', 'BrunswickRv_SEUSdb', 'StMarysRv_SEUSdb'
+        ],
+        'datasets': {
+            'gh': {
+                'filename_template': "USGS_gh_{location}.csv",
+                'measurement': "gh",
+                'fields': [
+                    ["mean", "mean"],
+                    ["climatology", "climatology"],
+                    ["anomaly", "anomaly"]
+                ],
+                'tags': [
+                    ['parameter', 'gh'],
+                    ['source', 'USGS']
+                ],
+                'timeCol': "time"
+            }
+        }
+    },
+    'FGB': {
+        'locations': [
+            'FGBdb_MS', 'FGBdb_TX', 'FKdb', 'FWCdb_EFL', 'FWCdb_STL'
+        ],
+        'datasets': {
+            'disch': {
+                'filename_template': "USGS_disch_{location}.csv",
+                'measurement': "disch",
+                'fields': [
+                    ["mean", "mean"],
+                    ["climatology", "climatology"],
+                    ["anomaly", "anomaly"]
+                ],
+                'tags': [
+                    ['parameter', 'disch'],
+                    ['source', 'USGS']
+                ],
+                'timeCol': "time"
+            }
+        }
+    }
+}
+
 with DAG(
     'ingest_usgs_gauges',
     catchup=False,
@@ -23,45 +68,26 @@ with DAG(
     max_active_runs=2,
     default_args={
         "start_date": datetime(2023, 6, 20),
-        #'retries': 3,
-        #'retry_delay': timedelta(days=1),
+        # 'retries': 3,
+        # 'retry_delay': timedelta(days=1),
     },
 ) as dag:
-    from csv2influx import csv2influx
-    
-    GBUCKET_URL_PREFIX = "https://storage.googleapis.com/dashboards_csvs"
-    FILE_LIST = {
-        'gh': [
-            'SavannahRv_SEUSdb','HudsonCr_SEUSdb','AltamahaRv_SEUSdb','SatillaRv_SEUSdb',
-            'StJohnsRv_SEUSdb','OgeecheeRv_SEUSdb','BrunswickRv_SEUSdb','StMarysRv_SEUSdb'
-        ],
-        'disch': [
-            'FGBdb_MS', 'FGBdb_TX',
-            'FKdb',
-            'FWCdb_EFL', 'FWCdb_STL'
-        ]
-    }
-    # example fname: USGS_gh_SavannahRv_SEUSdb.csv
-    for param, locations in FILE_LIST.items():
-        for location in locations:
-            DATA_FNAME = f"USGS_{param}_{location}.csv"
-            PythonOperator(
-                task_id=f"ingest_usgs_{param}_{location}",
-                python_callable=csv2influx,
-                op_kwargs={
-                    'data_url': f"{GBUCKET_URL_PREFIX}/{DATA_FNAME}",
-                    'measurement': param,
-                    'fields': [
-                        ["mean", "mean"],
-                        ["climatology", "climatology"],
-                        ["anomaly", "anomaly"]
-                    ],
-                    'tags': [
-                        ['parameter', param],
-                        ['location', location],
-                        ['source', 'USGS']
-                    ],
-                    'timeCol': "time"
-                },
-            )
-
+    for region, region_data in USGS_DB_FILES.items():
+        # Use the region in the URL generation.
+        GBUCKET_URL_PREFIX = f"https://storage.googleapis.com/{region.lower()}_csv"
+        for dataset_name, ds in region_data['datasets'].items():
+            for location in region_data['locations']:
+                DATA_FNAME = ds['filename_template'].format(location=location)
+                task_id = f"{region}_{dataset_name}_{location}"
+                tags = ds.get('tags', []) + [['location', location]]
+                PythonOperator(
+                    task_id=task_id,
+                    python_callable=csv2influx,
+                    op_kwargs={
+                        'data_url': f"{GBUCKET_URL_PREFIX}/{DATA_FNAME}",
+                        'measurement': ds['measurement'],
+                        'fields': ds['fields'],
+                        'tags': tags,
+                        'timeCol': ds['timeCol']
+                    },
+                )
