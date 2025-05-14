d8a68ef8ec557c622263af54208a48e8852831b7
Tylar Murray <email@tylar.info>
2025-05-14 19:57:53 +0000

    rm misplaced fn and + notes for data sources at top

--- airflow/dags/ingest_oa.py
+++ airflow/dags/ingest_oa.py
@@ -1,8 +1,24 @@
 """
-Ingests previous year's OA data from NERRS NDBC.
+Ingest Ocean Acidifcation data.
 
-NOTE: This DAG uses QC'd "historical" data only.
-      More recent "real time" data is available : https://www.ndbc.noaa.gov/station_realtime.php?station=41008
+
+# === Data Sources
+## Real Time
+SECOORA portal | 2018-present
+  https://portal.secoora.org/#metadata/75594/station/data
+  Cannot Download(?)
+
+
+## HISTORIC
+SECOORA portal | -2019:
+  https://portal.secoora.org/#metadata/49365/station/data
+  Cannot Download(?)
+ERDDAP         |  -2019:
+  https://erddap.secoora.org/erddap/tabledap/gov_ornl_cdiac_graysrf_81w_31n.html
+NCEI           | -2020:
+  https://www.ncei.noaa.gov/data/oceans/ncei/ocads/data/0109904/
+NCEI OCADS     | 2006-2020:
+  https://www.ncei.noaa.gov/access/ocean-carbon-acidification-data-system/oceans/Moorings/Grays_Reef.html
 """
 import os
 
@@ -13,71 +29,6 @@ import requests, gzip, io
 import pandas as pd
 
 from dataframe_to_influx import dataframe_to_influx
-
-
-
-def ndbc_to_influx(ds, **op_kwargs):
-    """
-    Downloads the NDBC historical stdmet file for station_name at year=ds-1,
-    parses it into a DataFrame with a datetime column, and then calls csv_to_influx().
-    Requires provide_context=True on the PythonOperator so that `ds` is injected.
-    """
-    # 1) year = params.ds - 1yr
-    #    ds comes in as 'YYYY-MM-DD'
-    year = int(ds.split('-')[0]) - 1
-    op_kwargs['year'] = year
-
-    # 2) grab & uncompress data from
-    data_url = (
-        f"https://www.ndbc.noaa.gov/data/historical/stdmet/"
-        f"{op_kwargs['buoy_id']}{year}.txt.gz"
-    )
-    resp = requests.get(data_url)
-    resp.raise_for_status()
-
-    # 3) uncompress into a file-like object
-    compressed = io.BytesIO(resp.content)
-    with gzip.GzipFile(fileobj=compressed) as gz:
-        # 4) load as DataFrame: skip the 2nd header row (units) and drop any comment lines
-        df = pd.read_csv(
-            gz,
-            delim_whitespace=True,
-            skiprows=[1],      # drop the units line
-            #comment='#'        # skip any other comment lines
-        )
-        # first column (YY) gets read in with # prepended (#YY)
-        df['YY'] = df['#YY']
-
-    pandas.set_option('display.max_columns', None)
-    print('df.head:', df.head())
-    # 5) add 'datetime' column using YY, MM, DD, hh, mm
-    #    First build a 4-digit year column
-    df['datetime'] = pd.to_datetime({
-        'year':   df['YY'],
-        'month':  df['MM'],
-        'day':    df['DD'],
-        'hour':   df['hh'],
-        'minute': df['mm']
-    })
-
-    # override the time column name for csv_to_influx
-    op_kwargs['dataframe'] = df
-    op_kwargs['timeCol'] = 'datetime'
-
-
-    print('kwargs:', op_kwargs)
-    #    def dataframe_to_influx(dataframe, measurement, tags=[[]], fields=[["value", "Sal"]],
-    #               timeCol='DateTimeStamp', should_convert_time=False):
-    # finally hand off to your generic CSV→Influx helper
-    dataframe_to_influx(
-        df,
-        op_kwargs['measurement'],
-        op_kwargs['tags'],
-        op_kwargs['fields'],
-        op_kwargs['timeCol'],
-        #op_kwargs['should_convert_time']
-    )
-
 
 
 # ============================================================================
